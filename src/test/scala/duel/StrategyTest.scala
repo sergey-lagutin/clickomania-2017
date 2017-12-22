8719b131b3135a94ae1332c39ac191b4d945992e
sergey-lagutin <sergey.v.lagutin@gmail.com>
2017-12-22 10:21:04 +0300

    Refactor StrategyTest.

--- src/test/scala/duel/StrategyTest.scala
+++ src/test/scala/duel/StrategyTest.scala
@@ -1,5 +1,6 @@
 package duel
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
@@ -7,12 +8,25 @@ object StrategyTest {
 
   trait Strategy extends (Seq[Component] => Seq[Component])
 
-  def strategies: Array[Strategy] =
+  def strategies: Array[AnyRef] =
     Array(
-      doNothing
+      Array(doNothing, "doNothing"),
+      Array(byMinColor, "byMinColor"),
+      Array(byMaxColor, "byMaxColor")
     )
 
   def doNothing: Strategy = (cs: Seq[Component]) => cs
+
+  def byMinColor: Strategy = (cs: Seq[Component]) =>
+    cs.sorted(colorOrdering(cs))
+
+  private def colorOrdering(cs: Seq[Component]): Ordering[Component] = {
+    val colorMap = cs.groupBy(_.color).mapValues(_.size)
+    (x: Component, y: Component) => colorMap(x.color) - colorMap(y.color)
+  }
+
+  def byMaxColor: Strategy = (cs: Seq[Component]) =>
+    cs.sorted(colorOrdering(cs).reverse)
 }
 
 class StrategyTest {
@@ -20,23 +34,36 @@ class StrategyTest {
 
   @MethodSource(Array("strategies"))
   @ParameterizedTest
-  def testFive(strategy: Seq[Component] => Seq[Component]): Unit = {
+  def testFive(strategy: Seq[Component] => Seq[Component], name: String): Unit = {
     val boards = Boards.five.map(array => new Board(5, array, strategy))
-    estimate(boards)
+    estimate(10, boards, name)
   }
 
   @MethodSource(Array("strategies"))
   @ParameterizedTest
-  def testSix(strategy: Seq[Component] => Seq[Component]): Unit = {
+  @Disabled
+  def testSix(strategy: Seq[Component] => Seq[Component], name: String): Unit = {
     val boards = Boards.six.take(1).map(array => new Board(6, array, strategy))
-    estimate(boards)
+    estimate(10, boards, name)
   }
 
-  private def estimate(boards: Seq[Board]): Unit = {
-    val start = System.currentTimeMillis()
-    boards.foreach(solver.findSolution)
-    val end = System.currentTimeMillis()
-    println(s"time: ${end - start} ms")
+  private def estimate(testAmount: Int, boards: Seq[Board], name: String): Unit = {
+    var min = Long.MaxValue
+    var max = Long.MinValue
+    var total = 0L
+
+    for {
+      _ <- 1 to testAmount
+    } {
+      val start = System.currentTimeMillis()
+      boards.foreach(solver.findSolution)
+      val end = System.currentTimeMillis()
+      val time = end - start
+      if (time < min) min = time
+      if (time > max) max = time
+      total += time
+    }
+    println(s"$name avg: ${total / testAmount}; min: $min; max: $max ms")
   }
 
 }
